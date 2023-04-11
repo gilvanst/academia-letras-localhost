bec6035545ca42bb5dd2cbcd20d5d9c2a1026984
Rubyzzs <paty_dml@hotmail.com>
2023-04-11 17:02:36 -0300

    nada

--- funcoes/conexao.php
+++ funcoes/conexao.php
@@ -1,8 +1,8 @@
 <?php
-    $tabela = "aracal41_bdaal";
-    $usuario = "aracal41_gilvan";
-    $senha = "junior003";
-    $host = "localhost";
+     $tabela = "bdaal";
+     $usuario = "root";
+     $senha = "";
+     $host = "localhost";
 
     $conexaoBanco = new PDO("mysql:host={$host};dbname={$tabela};charset=utf8", $usuario, $senha);
     $GLOBALS['conexaoBanco'] = $conexaoBanco;
